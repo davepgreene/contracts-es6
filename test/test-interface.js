54f5d62f3d3c677ee2049168b440f517781b97e4
Dave Greene <davepgreene@gmail.com>
2017-09-03 14:18:06 -0400

    Attached the specified mode to the generated Interface class. This lets us simplify the signature for Interface.check because we have access to the interface's mode already.

--- test/test-interface.js
+++ test/test-interface.js
@@ -15,13 +15,13 @@ test('Strict Mode - Returns false if all methods are not implemented', (t) => {
 });
 
 test('Loose Mode - Returns false if all methods are not implemented', (t) => {
-  const valid = Interface.check(TestLooseImpl, false);
+  const valid = Interface.check(TestLooseImpl);
   t.is(valid, false);
 });
 
 test('Strict Mode - Returns false if all methods are not implemented in the prototype chain', (t) => {
   TestStrictImpl.prototype.method1 = () => { };
-  const valid = Interface.check(TestInheritStrictImpl, true);
+  const valid = Interface.check(TestInheritStrictImpl);
   t.is(valid, false);
 });
 
@@ -30,13 +30,13 @@ test('Strict Mode - Returns true if all methods are implemented in the prototype
   TestInheritStrictImpl.prototype.method3WithParams = (foo, bar, baz) => { };
   TestInheritMoreStrictImpl.prototype.method2 = () => { };
 
-  const valid = Interface.check(TestInheritMoreStrictImpl, true);
+  const valid = Interface.check(TestInheritMoreStrictImpl);
   t.is(valid, true);
 });
 
 test('Loose Mode - Returns false if all methods are not implemented in the prototype chain', (t) => {
   TestLooseImpl.prototype.method1 = () => { };
-  const valid = Interface.check(TestInheritLooseImpl, false);
+  const valid = Interface.check(TestInheritLooseImpl);
   t.is(valid, false);
 });
 
@@ -54,6 +54,6 @@ test('Loose Mode - Returns true if all methods are implemented', (t) => {
   TestLooseImpl.prototype.method2 = () => { };
   TestLooseImpl.prototype.method3WithParams = (foo) => { };
 
-  const valid = Interface.check(TestLooseImpl, false);
+  const valid = Interface.check(TestLooseImpl);
   t.is(valid, true);
 });
