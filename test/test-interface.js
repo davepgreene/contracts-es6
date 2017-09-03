0391b6654fe84cc1e74c28f2f6ac90c39af0adab
Dave Greene <davepgreene@gmail.com>
2017-09-02 20:04:00 -0400

    Added method to check whether a class implements its interface. This is done without instantiating an instance of the class and is appropriate to use at application boot time. Added tests.

---
+++ test/test-interface.js
@@ -0,0 +1,59 @@
+import test from 'ava';
+
+import Interface from '../lib/interface';
+import { TestStrictImpl, TestInheritStrictImpl, TestLooseImpl, TestInheritLooseImpl, reset } from './_consts';
+
+class TestInheritMoreStrictImpl extends TestInheritStrictImpl { }
+class TestInheritMoreLooseImpl extends TestInheritLooseImpl { }
+
+// Call this at the end of every test
+test.afterEach.always(reset);
+
+test('Strict Mode - Returns false if all methods are not implemented', (t) => {
+  const valid = Interface.check(TestStrictImpl);
+  t.is(valid, false);
+});
+
+test('Loose Mode - Returns false if all methods are not implemented', (t) => {
+  const valid = Interface.check(TestLooseImpl, false);
+  t.is(valid, false);
+});
+
+test('Strict Mode - Returns false if all methods are not implemented in the prototype chain', (t) => {
+  TestStrictImpl.prototype.method1 = () => { };
+  const valid = Interface.check(TestInheritStrictImpl, true);
+  t.is(valid, false);
+});
+
+test('Strict Mode - Returns true if all methods are implemented in the prototype chain', (t) => {
+  TestStrictImpl.prototype.method1 = () => { };
+  TestInheritStrictImpl.prototype.method3WithParams = (foo, bar, baz) => { };
+  TestInheritMoreStrictImpl.prototype.method2 = () => { };
+
+  const valid = Interface.check(TestInheritMoreStrictImpl, true);
+  t.is(valid, true);
+});
+
+test('Loose Mode - Returns false if all methods are not implemented in the prototype chain', (t) => {
+  TestLooseImpl.prototype.method1 = () => { };
+  const valid = Interface.check(TestInheritLooseImpl, false);
+  t.is(valid, false);
+})
+
+test('Strict Mode - Returns true if all methods are implemented correctly', (t) => {
+  TestStrictImpl.prototype.method1 = () => { };
+  TestStrictImpl.prototype.method2 = () => { };
+  TestStrictImpl.prototype.method3WithParams = (foo, bar, baz) => { };
+
+  const valid = Interface.check(TestStrictImpl);
+  t.is(valid, true);
+});
+
+test('Loose Mode - Returns true if all methods are implemented', (t) => {
+  TestLooseImpl.prototype.method1 = () => { };
+  TestLooseImpl.prototype.method2 = () => { };
+  TestLooseImpl.prototype.method3WithParams = (foo) => { };
+
+  const valid = Interface.check(TestLooseImpl, false);
+  t.is(valid, true);
+});
